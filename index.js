2e0b47972252246dbc5235fa08740bb723ba361f
Pavel Zhelnov <pzhelnov@modeln.com>
2026-02-01 19:15:26 +0200

    stage 1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+// Налаштовуємо CORS, щоб ваш сайт на Vercel міг підключитися
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('Гравець підключився:', socket.id);
+
+    socket.on('join_room', (roomId) => {
+        socket.join(roomId);
+        console.log(`Гравець ${socket.id} зайшов у кімнату ${roomId}`);
+    });
+
+    socket.on('move', (data) => {
+        // Пересилаємо хід іншому гравцю в цій же кімнаті
+        socket.to(data.roomId).emit('opponent_move', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Гравець відключився');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Сервер запущено на порту ${PORT}`);
+});
